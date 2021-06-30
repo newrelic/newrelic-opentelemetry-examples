aa8634baf55111efd7e51f7055e0500591d5c893
Chris Ventura <45495992+nrcventura@users.noreply.github.com>
2021-06-30 13:28:09 -0700

    Enable Serilog

--- dotnet/logs-in-context/Program.cs
+++ dotnet/logs-in-context/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace logs_in_context
 {
@@ -13,11 +14,17 @@ namespace logs_in_context
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
