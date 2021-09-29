05531790b000adbd9f51f463011e68c60e13d470
jack-berg <34418638+jack-berg@users.noreply.github.com>
2021-09-29 10:21:31 -0500

    Simplify otel java SDK config (#51)

--- java/shared-utils/src/main/java/com/newrelic/shared/OpenTelemetryConfig.java
+++ java/shared-utils/src/main/java/com/newrelic/shared/OpenTelemetryConfig.java
@@ -8,8 +8,6 @@ import static io.opentelemetry.sdk.metrics.common.InstrumentType.UP_DOWN_COUNTER
 import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_INSTANCE_ID;
 import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.GlobalMeterProvider;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
@@ -24,7 +22,6 @@ import io.opentelemetry.sdk.metrics.aggregator.AggregatorFactory;
 import io.opentelemetry.sdk.metrics.common.InstrumentType;
 import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
 import io.opentelemetry.sdk.metrics.export.IntervalMetricReader;
-import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import io.opentelemetry.sdk.metrics.processor.LabelsProcessorFactory;
 import io.opentelemetry.sdk.metrics.view.InstrumentSelector;
 import io.opentelemetry.sdk.metrics.view.View;
@@ -43,44 +40,21 @@ public class OpenTelemetryConfig {
   private static final Supplier<String> NEW_RELIC_API_KEY_SUPPLIER =
       getEnvOrDefault("NEW_RELIC_API_KEY", Function.identity(), "");
   private static final Supplier<Boolean> LOG_EXPORTER_ENABLED =
-      getEnvOrDefault("LOG_EXPORTER_ENABLED", Boolean::valueOf, false);
+      getEnvOrDefault("LOG_EXPORTER_ENABLED", Boolean::valueOf, true);
 
   public static void configureGlobal(String defaultServiceName) {
-    var serviceName =
-        getEnvOrDefault("SERVICE_NAME", Function.identity(), defaultServiceName).get();
+    var resource =
+        Resource.builder()
+            .put(
+                SERVICE_NAME,
+                getEnvOrDefault("SERVICE_NAME", Function.identity(), defaultServiceName).get())
+            .put(SERVICE_INSTANCE_ID, UUID.randomUUID().toString())
+            .build();
+
     // Configure traces
-    GlobalOpenTelemetry.set(openTelemetrySdk(serviceName));
-
-    // Configure metrics
-    SdkMeterProvider sdkMeterProvider = meterProvider(serviceName);
-    GlobalMeterProvider.set(sdkMeterProvider);
-    intervalMetricReader(sdkMeterProvider, otlpMetricExporter()).start();
-    if (LOG_EXPORTER_ENABLED.get()) {
-      intervalMetricReader(sdkMeterProvider, new LoggingMetricExporter()).start();
-    }
-  }
-
-  public static Resource resource(String serviceName) {
-    return Resource.getDefault()
-        .merge(
-            Resource.create(
-                Attributes.builder()
-                    .put(SERVICE_NAME, serviceName)
-                    .put(SERVICE_INSTANCE_ID, UUID.randomUUID().toString())
-                    .build()));
-  }
-
-  public static OpenTelemetrySdk openTelemetrySdk(String serviceName) {
-    return OpenTelemetrySdk.builder()
-        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-        .setTracerProvider(tracerProvider(serviceName))
-        .build();
-  }
-
-  public static SdkTracerProvider tracerProvider(String serviceName) {
     var sdkTracerProviderBuilder =
         SdkTracerProvider.builder()
-            .setResource(resource(serviceName))
+            .setResource(resource)
             .addSpanProcessor(
                 BatchSpanProcessor.builder(
                         OtlpGrpcSpanExporter.builder()
@@ -89,27 +63,41 @@ public class OpenTelemetryConfig {
                                     OTLP_HOST_SUPPLIER.get(), NEW_RELIC_API_KEY_SUPPLIER.get()))
                             .build())
                     .build());
-
     if (LOG_EXPORTER_ENABLED.get()) {
       sdkTracerProviderBuilder.addSpanProcessor(
           BatchSpanProcessor.builder(new LoggingSpanExporter()).build());
     }
+    OpenTelemetrySdk.builder()
+        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+        .setTracerProvider(sdkTracerProviderBuilder.build())
+        .buildAndRegisterGlobal();
 
-    var sdkTracerProvider = sdkTracerProviderBuilder.build();
-
-    Runtime.getRuntime().addShutdownHook(new Thread(sdkTracerProvider::close));
-
-    return sdkTracerProvider;
-  }
-
-  public static SdkMeterProvider meterProvider(String serviceName) {
-    var meterProviderBuilder = SdkMeterProvider.builder().setResource(resource(serviceName));
+    // Configure metrics
+    var meterProviderBuilder = SdkMeterProvider.builder().setResource(resource);
     // Override default cumulative sum aggregator with delta sum aggregator
     setDeltaSumAggregatorFactory(meterProviderBuilder, COUNTER);
     setDeltaSumAggregatorFactory(meterProviderBuilder, UP_DOWN_COUNTER);
     setDeltaSumAggregatorFactory(meterProviderBuilder, OBSERVABLE_SUM);
     setDeltaSumAggregatorFactory(meterProviderBuilder, OBSERVABLE_UP_DOWN_SUM);
-    return meterProviderBuilder.build();
+    SdkMeterProvider sdkMeterProvider = meterProviderBuilder.build();
+    GlobalMeterProvider.set(sdkMeterProvider);
+    IntervalMetricReader.builder()
+        .setMetricExporter(
+            OtlpGrpcMetricExporter.builder()
+                .setChannel(
+                    OtlpUtil.managedChannel(
+                        OTLP_HOST_SUPPLIER.get(), NEW_RELIC_API_KEY_SUPPLIER.get()))
+                .build())
+        .setExportIntervalMillis(5000)
+        .setMetricProducers(List.of(sdkMeterProvider))
+        .buildAndStart();
+    if (LOG_EXPORTER_ENABLED.get()) {
+      IntervalMetricReader.builder()
+          .setMetricExporter(new LoggingMetricExporter())
+          .setExportIntervalMillis(5000)
+          .setMetricProducers(List.of(sdkMeterProvider))
+          .buildAndStart();
+    }
   }
 
   private static void setDeltaSumAggregatorFactory(
@@ -120,27 +108,6 @@ public class OpenTelemetryConfig {
             .setAggregatorFactory(AggregatorFactory.sum(AggregationTemporality.DELTA))
             .setLabelsProcessorFactory(LabelsProcessorFactory.noop())
             .build());
-  }
-
-  public static OtlpGrpcMetricExporter otlpMetricExporter() {
-    return OtlpGrpcMetricExporter.builder()
-        .setChannel(
-            OtlpUtil.managedChannel(OTLP_HOST_SUPPLIER.get(), NEW_RELIC_API_KEY_SUPPLIER.get()))
-        .build();
-  }
-
-  public static IntervalMetricReader intervalMetricReader(
-      SdkMeterProvider meterProvider, MetricExporter metricExporter) {
-    var reader =
-        IntervalMetricReader.builder()
-            .setMetricExporter(metricExporter)
-            .setExportIntervalMillis(5000)
-            .setMetricProducers(List.of(meterProvider))
-            .build();
-
-    Runtime.getRuntime().addShutdownHook(new Thread(reader::shutdown));
-
-    return reader;
   }
 
   private OpenTelemetryConfig() {}
