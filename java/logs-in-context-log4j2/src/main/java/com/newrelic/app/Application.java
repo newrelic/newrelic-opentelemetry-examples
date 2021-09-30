e2619cb8a4b66fa2876ee392366c5741a59cad90
jack-berg <jberg@newrelic.com>
2021-09-30 16:16:30 -0500

    Update logs-in-context-log4j2 example to point directly to new relic

--- java/logs-in-context-log4j2/src/main/java/com/newrelic/app/Application.java
+++ java/logs-in-context-log4j2/src/main/java/com/newrelic/app/Application.java
@@ -1,5 +1,6 @@
 package com.newrelic.app;
 
+import com.newrelic.shared.OpenTelemetryConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
+    OpenTelemetryConfig.configureGlobal(System.getenv("SERVICE_NAME"));
     SpringApplication.run(Application.class, args);
   }
 }
