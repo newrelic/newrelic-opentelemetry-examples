18a175a1ead240306fc87c6ea56d1bf3fede30fa
jack-berg <jberg@newrelic.com>
2021-10-05 17:15:01 -0500

    Add standalone library instrumentation to java sdk-nr-config example

--- java/sdk-nr-config/src/main/java/com/newrelic/app/Application.java
+++ java/sdk-nr-config/src/main/java/com/newrelic/app/Application.java
@@ -1,15 +1,31 @@
 package com.newrelic.app;
 
 import com.newrelic.shared.OpenTelemetryConfig;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.runtimemetrics.GarbageCollector;
+import io.opentelemetry.instrumentation.runtimemetrics.MemoryPools;
+import io.opentelemetry.instrumentation.spring.webmvc.WebMvcTracingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     // Configure OpenTelemetry as early as possible
-    OpenTelemetryConfig.configureGlobal("sdk-nr-config");
+    OpenTelemetryConfig.configureGlobal("sdk-nr-config-");
+
+    // Register runtime metrics instrumentation
+    MemoryPools.registerObservers();
+    GarbageCollector.registerObservers();
+
     SpringApplication.run(Application.class, args);
+  }
+
+  /** Add Spring WebMVC instrumentation by registering tracing filter. */
+  @Bean
+  public WebMvcTracingFilter webMvcTracingFilter() {
+    return new WebMvcTracingFilter(GlobalOpenTelemetry.get());
   }
 }
--- java/sdk-nr-config/src/main/java/com/newrelic/app/Controller.java
+++ java/sdk-nr-config/src/main/java/com/newrelic/app/Controller.java
@@ -6,9 +6,7 @@ import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.GlobalMeterProvider;
 import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import java.util.Random;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,19 +22,11 @@ public class Controller {
 
   @GetMapping("/ping")
   public String ping() throws InterruptedException {
-    var span =
-        TRACER
-            .spanBuilder("/ping")
-            .setAttribute(SemanticAttributes.HTTP_METHOD, "GET")
-            .setAttribute(SemanticAttributes.HTTP_SCHEME, "http")
-            .setAttribute(SemanticAttributes.HTTP_HOST, "localhost:8080")
-            .setAttribute(SemanticAttributes.HTTP_TARGET, "/ping")
-            .setSpanKind(SpanKind.SERVER)
-            .startSpan();
+    var span = TRACER.spanBuilder("ping").startSpan();
     try (var scope = span.makeCurrent()) {
       var sleepTime = new Random().nextInt(200);
       Thread.sleep(sleepTime);
-      MY_COUNTER.add(sleepTime, Attributes.of(AttributeKey.stringKey("path"), "/ping"));
+      MY_COUNTER.add(sleepTime, Attributes.of(AttributeKey.stringKey("method"), "ping"));
       return "pong";
     } finally {
       span.end();
