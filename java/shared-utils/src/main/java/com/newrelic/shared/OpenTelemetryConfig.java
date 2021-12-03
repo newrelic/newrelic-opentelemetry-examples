62c02c7e5cc7e5ff25415976b741db3f53ec0223
jack-berg <34418638+jack-berg@users.noreply.github.com>
2021-12-02 19:43:58 -0600

    Demonstrate log SDK with log4j2 (#76)
    * Update to otel 1.9.x
    
    * Add log support to sdk-nr-config
    
    * Try to appease snyk

--- java/shared-utils/src/main/java/com/newrelic/shared/OpenTelemetryConfig.java
+++ java/shared-utils/src/main/java/com/newrelic/shared/OpenTelemetryConfig.java
@@ -1,11 +1,6 @@
 package com.newrelic.shared;
 
 import static com.newrelic.shared.EnvUtils.getEnvOrDefault;
-import static io.opentelemetry.sdk.metrics.common.InstrumentType.COUNTER;
-import static io.opentelemetry.sdk.metrics.common.InstrumentType.HISTOGRAM;
-import static io.opentelemetry.sdk.metrics.common.InstrumentType.OBSERVABLE_SUM;
-import static io.opentelemetry.sdk.metrics.common.InstrumentType.OBSERVABLE_UP_DOWN_SUM;
-import static io.opentelemetry.sdk.metrics.common.InstrumentType.UP_DOWN_COUNTER;
 import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_INSTANCE_ID;
 import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
 
@@ -14,17 +9,15 @@ import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.logging.LoggingMetricExporter;
 import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogExporter;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLogEmitterProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogProcessor;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
-import io.opentelemetry.sdk.metrics.common.InstrumentType;
 import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.sdk.metrics.view.Aggregation;
-import io.opentelemetry.sdk.metrics.view.InstrumentSelector;
-import io.opentelemetry.sdk.metrics.view.View;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
@@ -45,15 +38,7 @@ public class OpenTelemetryConfig {
       getEnvOrDefault("LOG_EXPORTER_ENABLED", Boolean::valueOf, true);
 
   public static void configureGlobal(String defaultServiceName) {
-    var serviceName =
-        getEnvOrDefault("SERVICE_NAME", Function.identity(), defaultServiceName).get();
-    var resource =
-        Resource.getDefault()
-            .merge(
-                Resource.builder()
-                    .put(SERVICE_NAME, serviceName)
-                    .put(SERVICE_INSTANCE_ID, UUID.randomUUID().toString())
-                    .build());
+    var resource = configureResource(defaultServiceName);
 
     // Configure traces
     var sdkTracerProviderBuilder =
@@ -78,44 +63,50 @@ public class OpenTelemetryConfig {
 
     // Configure metrics
     var meterProviderBuilder = SdkMeterProvider.builder().setResource(resource);
-    // Override default cumulative aggregators with delta
-    setAggregation(meterProviderBuilder, COUNTER, Aggregation.sum(AggregationTemporality.DELTA));
-    setAggregation(
-        meterProviderBuilder, UP_DOWN_COUNTER, Aggregation.sum(AggregationTemporality.DELTA));
-    setAggregation(
-        meterProviderBuilder, OBSERVABLE_SUM, Aggregation.sum(AggregationTemporality.DELTA));
-    setAggregation(
-        meterProviderBuilder,
-        OBSERVABLE_UP_DOWN_SUM,
-        Aggregation.sum(AggregationTemporality.DELTA));
-    setAggregation(
-        meterProviderBuilder,
-        HISTOGRAM,
-        Aggregation.explicitBucketHistogram(AggregationTemporality.DELTA));
 
     meterProviderBuilder.registerMetricReader(
-        PeriodicMetricReader.create(
-            OtlpGrpcMetricExporter.builder()
-                .setChannel(
-                    OtlpUtil.managedChannel(OTLP_HOST_SUPPLIER.get(), newRelicApiOrLicenseKey()))
-                .build(),
-            Duration.ofSeconds(5)));
+        PeriodicMetricReader.builder(
+                OtlpGrpcMetricExporter.builder()
+                    .setPreferredTemporality(AggregationTemporality.DELTA)
+                    .setChannel(
+                        OtlpUtil.managedChannel(
+                            OTLP_HOST_SUPPLIER.get(), newRelicApiOrLicenseKey()))
+                    .build())
+            .setInterval(Duration.ofSeconds(5))
+            .newMetricReaderFactory());
 
     if (LOG_EXPORTER_ENABLED.get()) {
       meterProviderBuilder.registerMetricReader(
-          PeriodicMetricReader.create(new LoggingMetricExporter(), Duration.ofSeconds(5)));
+          PeriodicMetricReader.builder(new LoggingMetricExporter())
+              .setInterval(Duration.ofSeconds(5))
+              .newMetricReaderFactory());
     }
 
     GlobalMeterProvider.set(meterProviderBuilder.build());
   }
 
-  private static void setAggregation(
-      SdkMeterProviderBuilder meterProviderBuilder,
-      InstrumentType instrumentType,
-      Aggregation aggregation) {
-    meterProviderBuilder.registerView(
-        InstrumentSelector.builder().setInstrumentType(instrumentType).build(),
-        View.builder().setAggregation(aggregation).build());
+  private static Resource configureResource(String defaultServiceName) {
+    return Resource.getDefault()
+        .merge(
+            Resource.builder()
+                .put(
+                    SERVICE_NAME,
+                    getEnvOrDefault("SERVICE_NAME", Function.identity(), defaultServiceName).get())
+                .put(SERVICE_INSTANCE_ID, UUID.randomUUID().toString())
+                .build());
+  }
+
+  public static SdkLogEmitterProvider configureLogSdk(String defaultServiceName) {
+    return SdkLogEmitterProvider.builder()
+        .setResource(configureResource(defaultServiceName))
+        .addLogProcessor(
+            BatchLogProcessor.builder(
+                    OtlpGrpcLogExporter.builder()
+                        .setEndpoint(OTLP_HOST_SUPPLIER.get())
+                        .addHeader("api-key", newRelicApiOrLicenseKey())
+                        .build())
+                .build())
+        .build();
   }
 
   private static String newRelicApiOrLicenseKey() {
