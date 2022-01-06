d02fb900f7033172c4211be8010f9eaea2c2f1fd
Reese <reeselee.cy@gmail.com>
2022-01-06 14:20:41 -0800

    Add Python directory and initial app file

---
+++ appathon/python/app.py
@@ -0,0 +1,12 @@
+from markupsafe import escape
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
