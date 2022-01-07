462f2552c6401c67f2c6df7dd101d8b7db4d039c
Svetlana Brennan <svetlana.svn@gmail.com>
2022-01-07 14:58:59 -0600

    Add span error handling

--- appathon/javascript/index.js
+++ appathon/javascript/index.js
@@ -1,4 +1,4 @@
-const { trace, context } = require('@opentelemetry/api');
+const { trace, context, SpanStatusCode } = require('@opentelemetry/api');
 
 const express = require("express");
 const app = express();
@@ -7,40 +7,51 @@ const PORT = process.env.PORT || 8080;
 
 app.get("/fibonacci", (req, res) => {
   let n = Number(req.query.n);
-  
-  if (!n || n < 1|| n > 90) {
-    try {
-      throw new Error("n must be 1 <= n <= 90");
-    } catch (error) {
-      res.status(400).send({"message": error.message });
-    }
-  } else {
+
+  try {
     res.status(200).send({
       "n": n, 
       "result": fibonacci(n)
     });
+  } catch (error) {
+    res.status(400).send({"message": error.message });
   }
 });
 
-function fibonacci(element) {
+function fibonacci(input) {
   let currentSpan = trace.getSpan(context.active());
-  let tracer = trace.getTracer("fibonacci");
   let ctx = trace.setSpan(context.active(), currentSpan);
+  
+  let tracer = trace.getTracer("fibonacci");
   let span = tracer.startSpan("fibonacci", undefined, ctx);
+  span.setAttribute("n", input);
 
-  let sequence = [0, 1];
+  try {
+    if (!input || input < 1|| input > 90) {
+        throw new Error("n must be 1 <= n <= 90");
+    } else {
+      let sequence = [0, 1];
 
-  for (i = 2; i <= element; i++) {
-      sequence[i] = sequence[i - 2] + sequence[i - 1];
+      for (i = 2; i <= input; i++) {
+          sequence[i] = sequence[i - 2] + sequence[i - 1];
+      }
+
+      let result = sequence[input];
+
+      span.setStatus({ code: SpanStatusCode.OK });
+      span.setAttribute("result", result);
+    }
+  } catch(error) {
+    span.setStatus({
+      code: SpanStatusCode.ERROR,
+      message: error.message
+    });
+
+    span.recordException(error);
+    throw error;
+  } finally {
+    span.end();
   }
-
-  let result = sequence[element];
-
-  span.setAttribute("n", element)
-  span.setAttribute("result", result);
-  span.end();
-
-  return result;
 }
 
 app.listen(parseInt(PORT, 10), () => {
