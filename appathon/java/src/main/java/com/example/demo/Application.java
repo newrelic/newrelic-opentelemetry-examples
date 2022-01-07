ec82c90e4fb5afe628da4eca050b9261c6a9a0bb
jack-berg <jberg@newrelic.com>
2022-01-07 12:00:29 -0600

    Remove metrics from java

--- appathon/java/src/main/java/com/example/demo/Application.java
+++ appathon/java/src/main/java/com/example/demo/Application.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.instrumentation.runtimemetrics.GarbageCollector;
-import io.opentelemetry.instrumentation.runtimemetrics.MemoryPools;
 import io.opentelemetry.instrumentation.spring.webmvc.SpringWebMvcTracing;
 import io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 import javax.servlet.Filter;
 
 @SpringBootApplication
