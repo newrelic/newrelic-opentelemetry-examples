920bcbf206d2289b1d632e656e94d6db7d65e23d
Svetlana Brennan <svetlana.svn@gmail.com>
2022-01-07 14:13:10 -0600

    Updated tracing

--- appathon/javascript/index.js
+++ appathon/javascript/index.js
@@ -1,4 +1,5 @@
-let fibonacci = require("./fib");
+const { trace, context } = require('@opentelemetry/api');
+
 const express = require("express");
 const app = express();
 
@@ -7,17 +8,40 @@ const PORT = process.env.PORT || 8080;
 app.get("/fibonacci", (req, res) => {
   let n = Number(req.query.n);
   
-  if (!n || n < 1 || n > 1000) {
-    res.status(400).send({"message": "n must be >= 1 and <= 1000"});
+  if (!n || n > 90) {
+    try {
+      throw new Error("n must be 1 <= n <= 90");
+    } catch (error) {
+      res.status(400).send({"message": error.message });
+    }
   } else {
-    let fib = fibonacci(n);
-  
     res.status(200).send({
       "n": n, 
-      "result": fib
+      "result": fibonacci(n)
     });
   }
 });
+
+function fibonacci(element) {
+  let currentSpan = trace.getSpan(context.active());
+  let tracer = trace.getTracer("fibonacci");
+  let ctx = trace.setSpan(context.active(), currentSpan);
+  let span = tracer.startSpan("fibonacci", undefined, ctx);
+
+  let sequence = [0, 1];
+
+  for (i = 2; i <= element; i++) {
+      sequence[i] = sequence[i - 2] + sequence[i - 1];
+  }
+
+  let result = sequence[element];
+
+  span.setAttribute("n", element)
+  span.setAttribute("result", result);
+  span.end();
+
+  return result;
+}
 
 app.listen(parseInt(PORT, 10), () => {
   console.log(`Listening for requests on http://localhost:${PORT}`);
