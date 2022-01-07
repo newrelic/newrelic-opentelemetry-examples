aa08c67ea741eb1f84ee64441235ceb739eee1c6
Svetlana Brennan <svetlana.svn@gmail.com>
2022-01-06 18:35:48 -0600

    WIP javascript appathon

---
+++ appathon/javascript/index.js
@@ -0,0 +1,24 @@
+let fibonacci = require("./fib");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/fibonacci", (req, res) => {
+  let n = Number(req.query.n);
+  
+  if (!n || n < 1 || n > 1000) {
+    res.status(400).send({"message": "n must be >= 1 and <= 1000"});
+  } else {
+    let fib = fibonacci(n);
+  
+    res.status(200).send({
+      "n": n, 
+      "result": fib
+    });
+  }
+});
+
+app.listen(parseInt(PORT, 10), () => {
+  console.log(`Listening for requests on http://localhost:${PORT}`);
+})
