e49ff08c143b7c54d1afd37cdace0a68fa232f36
Alan West <3676547+alanwest@users.noreply.github.com>
2022-08-24 14:58:07 -0700

    Remove minimal api example

--- dotnet/minimal-api/Fruit.cs
+++
@@ -1,7 +0,0 @@
-namespace minimal_api;
-
-public class Fruit
-{
-    public static string[] SampleNames = new string[] {"Kiwi", "Apple", "Banana", "Pear", "Orange", "Grapefruit", "Dragonfruit", "Lime", "Lemon", "Watermelon"};
-    public string Name { get; set; }
-}
