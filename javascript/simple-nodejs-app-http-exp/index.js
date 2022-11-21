e9bfc1a2bd931acd343793422a29dd8180759e98
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-11-21 17:47:35 -0600

    Upgrade to otel js 1.3.0, 1.8.0, 0.34.0, 0.35.0 (#218)
    * Upgrade to otel js 1.3.0, 1.8.0, 0.34.0, 0.35.0
    
    * Cleanup readme
    
    * Cleanup root readme

--- javascript/simple-nodejs-app-http-exp/index.js
+++ javascript/simple-nodejs-app-http-exp/index.js
@@ -5,23 +5,11 @@ const api = require("@opentelemetry/api");
 
 const PORT = process.env.PORT || "8080";
 
-function getLargeValue() {
-  result = ""
-  for (i = 0; i < 4095; i++) {
-    result += "A"
-  }
-  result += "BBBBBBBBBBBBB"
-  return result
-}
-
-largeValue = getLargeValue()
-
 app.get("/ping", (_, res) => {
   const currentSpan = api.trace.getSpan(api.context.active());
 
-  // New Relic only accepts attributes values that are less than 4096 characters.
-  // When viewing this span in New Relic, the value of the "truncate" attribute will contain no Bs
-  currentSpan.setAttribute("truncate", largeValue)
+  // Add custom attribute
+  currentSpan.setAttribute("my-attribute", "my-attribute-value")
 
   res.status(200).send("Pong");
 });
--- javascript/simple-nodejs-app-http-exp/opentelemetry.js
+++ javascript/simple-nodejs-app-http-exp/opentelemetry.js
@@ -7,6 +7,8 @@ const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumenta
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
+const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-proto");
+const { AggregationTemporality, PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 const api = require("@opentelemetry/api");
 
 // enable logging ONLY for developement
@@ -18,7 +20,7 @@ api.diag.setLogger(
   api.DiagLogLevel.DEBUG,
 );
 
-// Step 1. Declare the resource to be used.
+// Declare the resource to be used.
 //    A resource represents a collection of attributes describing the
 //    service. This collection of attributes will be associated with all
 //    telemetry generated from this service (traces, metrics, logs).
@@ -27,13 +29,16 @@ const resource = new Resource({
   [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: uuidv4(),
 });
 
-// Step 2: Enable auto-instrumentation from the meta package.
+// Enable auto-instrumentation from the meta package.
 const instrumentations = [getNodeAutoInstrumentations()];
 
-// Step 3: Configure the OTLP/PROTO trace exporter.
+// Configure the OTLP/PROTO exporters.
 //    The following assumes you've set the OTEL_EXPORTER_OTLP_ENDPOINT and OTEL_EXPORTER_OLTP_HEADERS
 //    environment variables.
 const traceExporter = new OTLPTraceExporter();
+const metricExporter = new OTLPMetricExporter({
+  temporalityPreference: AggregationTemporality.DELTA
+});
 
 // If you haven't set the OTEL_EXPORTER_OTLP_ENDPOINT and OTEL_EXPORTER_OLTP_HEADERS
 // environment variables, you can configure the OTLP exporter programmatically by
@@ -51,26 +56,34 @@ const traceExporter = new OTLPTraceExporter();
 // }
 
 // const traceExporter = new OTLPTraceExporter(collectorOptions);
+// const metricExporter = new OTLPMetricExporter(collectorOptions);
 
-// // Step 4: Configure the OpenTelemetry NodeSDK to export traces.
+// Configure PeriodicMetricReader
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricExporter,
+  exportIntervalMillis: 5000,
+})
+
+// Configure the OpenTelemetry NodeSDK
 const sdk = new opentelemetry.NodeSDK({
   resource,
   traceExporter,
+  metricReader,
   instrumentations,
 });
 
-// Step 5: Initialize the SDK and register with the OpenTelemetry API:
+// Initialize the SDK and register with the OpenTelemetry API:
 //    this enables the API to record telemetry
 sdk
   .start()
-  .then(() => console.log("Tracing initialized"))
-  .catch((error) => console.log("Error initializing tracing", error));
+  .then(() => console.log("OpenTelemetry initialized"))
+  .catch((error) => console.log("Error initializing OpenTelemetry", error));
 
-// Step 6: Gracefully shut down the SDK on process exit
+// Gracefully shut down the SDK on process exit
 process.on("SIGTERM", () => {
   sdk
     .shutdown()
-    .then(() => console.log("Tracing terminated"))
-    .catch((error) => console.log("Error terminating tracing", error))
+    .then(() => console.log("OpenTelemetry terminated"))
+    .catch((error) => console.log("Error terminating OpenTelemetry", error))
     .finally(() => process.exit(0));
 });
