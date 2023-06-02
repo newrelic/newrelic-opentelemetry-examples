07bfa3df023ced147c2e3abc1c17d7bc6a1e9686
Reese Lee <reeselee.cy@gmail.com>
2023-06-02 15:37:15 -0700

    Update app.py
      * remove the service name from being hardcoded (I added an instruction to set service name as an env var in the README)
      * update the meter description
      * update the returned json response for a valid input
      * update the ValueError message 
      * remove the team tag `newrelic` 
      * remove the "Everything else" section, I moved the two items to the `Traces` and `Metrics` sections respectively

--- getting-started-guides/python/Instrumented/app.py
+++ getting-started-guides/python/Instrumented/app.py
@@ -11,10 +11,8 @@ from opentelemetry.sdk.resources import Resource
 import uuid
 
 OTEL_RESOURCE_ATTRIBUTES = {
-    "service.name": "getting-started-python", 
     "service.instance.id": str(uuid.uuid1()), 
-    "environment": "local",
-    "tags.team": "newrelic"
+    "environment": "local"
 }
 
 ##########
@@ -24,6 +22,7 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.trace.status import Status, StatusCode
 
 # Initialize tracing and an exporter that can send data to an OTLP endpoint
 # SELECT * FROM Span WHERE instrumentation.provider='opentelemetry'
@@ -42,6 +41,7 @@ from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExp
 # SELECT count(`http.server.active_requests`) FROM Metric FACET `service.name` TIMESERIES
 metrics.set_meter_provider(MeterProvider(resource=Resource.create(OTEL_RESOURCE_ATTRIBUTES), metric_readers=[PeriodicExportingMetricReader(OTLPMetricExporter())]))
 metrics.get_meter_provider()
+fib_counter = metrics.get_meter("opentelemetry.instrumentation.custom").create_counter("fibonacci.invocations", unit="1", description="Measures the number of times the fibonacci method is invoked.")
 
 ########
 # Logs # - OpenTelemetry Logs are still in the "experimental" state, so function names may change in the future
@@ -66,10 +66,6 @@ from opentelemetry.instrumentation.logging import LoggingInstrumentor
 FlaskInstrumentor().instrument_app(app)
 LoggingInstrumentor().instrument() 
 
-# Everything Else
-from opentelemetry.trace.status import Status, StatusCode
-fib_counter = metrics.get_meter("opentelemetry.instrumentation.custom").create_counter("fibonacci.invocations", unit="1", description="Counts the number of times this function runs")
-
 # The rest of the Flask application
 @app.route("/fibonacci/<int:x>", strict_slashes=False)
 @trace.get_tracer("opentelemetry.instrumentation.custom").start_as_current_span("fibonacci", kind=trace.SpanKind.SERVER)
@@ -85,12 +81,12 @@ def fibonacci(x):
         trace.get_current_span().set_attribute("fibonacci.result", array[x])
         fib_counter.add(1, {"fibonacci.valid.n": "true"})
         logging.info("Compute fibonacci(" + str(x) + ") = " + str(array[x]))
-        return jsonify(fibonacci_index=x, fibonacci_number=array[x])
+        return jsonify(n=x, result=array[x])
 
     except (ValueError, AssertionError):
         trace.get_current_span().set_status(Status(StatusCode.ERROR, "Number outside of accepted range."))
         fib_counter.add(1, {"fibonacci.valid.n": "false"})
         logging.error("Failed to compute fibonacci(" + str(x) + ")")
-        raise ValueError("x must be 1 <= x <= 90.")
+        raise ValueError("n must be 1 <= n <= 90.")
 
 app.run(host='0.0.0.0', port=8080)
