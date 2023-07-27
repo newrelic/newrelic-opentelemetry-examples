a2c49147af360693bf638d40ecb8a534d9656f21
matewilk <matewilk@gmail.com>
2023-07-27 12:26:38 +0100

    (feat): make auto node instrumentation trully automatic for metrics

--- getting-started-guides/javascript/instrumented/src/auto-otel-node.js
+++ getting-started-guides/javascript/instrumented/src/auto-otel-node.js
@@ -8,23 +8,21 @@ const {
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
-const {
-  MeterProvider,
-  PeriodicExportingMetricReader,
-} = require("@opentelemetry/sdk-metrics");
+const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
 const { Resource } = require("@opentelemetry/resources");
 const {
   SemanticResourceAttributes,
 } = require("@opentelemetry/semantic-conventions");
-const opentelemetry = require("@opentelemetry/api");
 
-const meterProvider = new MeterProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: `${process.env.OTEL_SERVICE_NAME_NODE}`,
-    [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: `${process.env.OTEL_SERVICE_NAME_NODE}-instance`,
-    [SemanticResourceAttributes.SERVICE_VERSION]: "1.0.0",
-  }),
+// Traces
+const traceExporter = new OTLPTraceExporter({
+  url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`,
+  headers: {
+    "api-key": `${process.env.NEW_RELIC_LICENSE_INGEST_KEY}`,
+  },
 });
+
+// Metrics
 const metricsExporter = new OTLPMetricExporter({
   url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/metrics`,
   headers: {
@@ -37,23 +35,15 @@ const metricReader = new PeriodicExportingMetricReader({
   exportIntervalMillis: 10000,
 });
 
-meterProvider.addMetricReader(metricReader);
-opentelemetry.metrics.setGlobalMeterProvider(meterProvider);
-
 const sdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: `${process.env.OTEL_SERVICE_NAME_NODE}`,
     [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: `${process.env.OTEL_SERVICE_NAME_NODE}-instance`,
     [SemanticResourceAttributes.SERVICE_VERSION]: "1.0.0",
   }),
-  traceExporter: new OTLPTraceExporter({
-    url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`,
-    headers: {
-      "api-key": `${process.env.NEW_RELIC_LICENSE_INGEST_KEY}`,
-    },
-  }),
+  traceExporter: traceExporter,
+  metricReader: metricReader,
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
-sdk.configureMeterProvider(meterProvider);
 sdk.start();
