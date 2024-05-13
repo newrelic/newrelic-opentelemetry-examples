1ce4c884360104ac657dd36e8597aeabb29efd59
Alan West <3676547+alanwest@users.noreply.github.com>
2024-05-13 14:05:55 -0700

    Put it all together with docker compose

---
+++ getting-started-guides/supporting-files/loadgenerator.py
@@ -0,0 +1,15 @@
+import os, random, sys, signal, time
+
+def signal_handler(signal, frame):
+    print("\nCtrl-C received. Stopping load generator.")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+languages = ["dotnet", "go", "javascript", "ruby"]
+
+while True:
+    n = random.randint(1, 100)
+    for language in languages:
+        os.system(f"curl http://{language}:8080/fibonacci?n={n} > /dev/null 2>&1")
+    time.sleep(1)
