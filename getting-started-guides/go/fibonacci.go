828a500ee1c100c2fccbd010045fcb01795c8fb1
Alan West <3676547+alanwest@users.noreply.github.com>
2024-06-13 15:33:24 -0700

    Overhaul Go example. Add logs and fix bugs

--- getting-started-guides/go/fibonacci.go
+++ getting-started-guides/go/fibonacci.go
@@ -3,19 +3,30 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 
+	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
-	"go.opentelemetry.io/otel/trace"
 )
 
 const (
+	name                      = "getting-started-go"
 	INPUT_COULD_NOT_BE_PARSED = "Input could not be parsed."
 	INPUT_IS_OUTSIDE_OF_RANGE = "Input is outside of the range [1,90]."
 	CALCULATION_SUCCEEDED     = "Fibonacci is calculated successfully."
+)
+
+var (
+	tracer               = otel.Tracer(name)
+	meter                = otel.Meter(name)
+	logger               = otelslog.NewLogger(name)
+	fibonacciInvocations metric.Int64Counter
 )
 
 type responseObject struct {
@@ -24,12 +35,18 @@ type responseObject struct {
 	Output  *int64 `json:"output"`
 }
 
-// HTTP handler for Fibonacci calculation
-func handler(
-	w http.ResponseWriter,
-	r *http.Request,
-) {
+func init() {
+	var err error
+	fibonacciInvocations, err = meter.Int64Counter(
+		"fibonacci.invocations",
+		metric.WithDescription("Measures the number of times the fibonacci method is invoked."),
+	)
+	if err != nil {
+		panic(err)
+	}
+}
 
+func fibonacci(w http.ResponseWriter, r *http.Request) {
 	// Parse input number
 	num, err := parseNum(r)
 	if err != nil {
@@ -68,12 +85,7 @@ func handler(
 		})
 }
 
-func parseNum(
-	r *http.Request,
-) (
-	int64,
-	error,
-) {
+func parseNum(r *http.Request) (int64, error) {
 	num, err := strconv.ParseInt(r.URL.Query().Get("n"), 10, 64)
 	if err != nil {
 		log.Print(err.Error())
@@ -81,31 +93,14 @@ func parseNum(
 	return num, err
 }
 
-// Calculate Fibonacci per given input number
-func calculateFibonacci(
-	r *http.Request,
-	n int64,
-) (
-	int64,
-	error,
-) {
-
-	// Start an internal child span for Fibonacci calculation
-	_, span := trace.SpanFromContext(r.Context()).
-		TracerProvider().
-		Tracer("Fibonacci").
-		Start(
-			r.Context(),
-			"fibonacci",
-			trace.WithSpanKind(trace.SpanKindInternal),
-		)
+func calculateFibonacci(r *http.Request, n int64) (int64, error) {
+	ctx, span := tracer.Start(r.Context(), "fibonacci")
 	defer span.End()
 
 	fibonacciSpanAttrs := []attribute.KeyValue{
 		attribute.Int64("fibonacci.n", n),
 	}
 
-	// Check input
 	if n <= 1 || n > 90 {
 		log.Print(INPUT_IS_OUTSIDE_OF_RANGE)
 
@@ -116,30 +111,30 @@ func calculateFibonacci(
 		)
 		span.SetAttributes(fibonacciSpanAttrs...)
 
+		fibonacciInvocations.Add(ctx, 1, metric.WithAttributes(attribute.Bool("fibonacci.valid.n", false)))
+
 		return 0, errors.New("invalid input")
 	}
 
-	// Calculate Fibonacci
 	var n2, n1 int64 = 0, 1
 	for i := int64(2); i < n; i++ {
 		n2, n1 = n1, n1+n2
 	}
 	res := n2 + n1
 
-	// Set calculation result into span
 	fibonacciSpanAttrs = append(fibonacciSpanAttrs,
 		attribute.Int64("fibonacci.result", res),
 	)
 	span.SetAttributes(fibonacciSpanAttrs...)
 
+	msg := fmt.Sprintf("Computed fib({%d}) = {%d}.", n, res)
+	logger.InfoContext(ctx, msg, "fibonacci.n", n, "fibonacci.result", res)
+	fibonacciInvocations.Add(ctx, 1, metric.WithAttributes(attribute.Bool("fibonacci.valid.n", true)))
+
 	return res, nil
 }
 
-func createHttpResponse(
-	w http.ResponseWriter,
-	statusCode int,
-	res *responseObject,
-) {
+func createHttpResponse(w http.ResponseWriter, statusCode int, res *responseObject) {
 	w.WriteHeader(statusCode)
 	payload, _ := json.Marshal(res)
 	w.Write(payload)
--- getting-started-guides/go/otel.go
+++ getting-started-guides/go/otel.go
@@ -2,234 +2,123 @@ package main
 
 import (
 	"context"
-	"log"
-	"net/http"
-	"os"
+	"errors"
 	"time"
 
+	"go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/metric"
-
+	"go.opentelemetry.io/otel/log/global"
 	"go.opentelemetry.io/otel/propagation"
-	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/metric/metricdata"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.19.0"
-	"go.opentelemetry.io/otel/semconv/v1.19.0/httpconv"
-	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
 )
 
-func newRelicTemporalitySelector(kind sdkmetric.InstrumentKind) metricdata.Temporality {
-	if kind == sdkmetric.InstrumentKindUpDownCounter || kind == sdkmetric.InstrumentKindObservableUpDownCounter {
-		return metricdata.CumulativeTemporality
-	}
-	return metricdata.DeltaTemporality
-}
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
 
-func newMetricProvider(
-	ctx context.Context,
-) *sdkmetric.MeterProvider {
-	var exp sdkmetric.Exporter
-	var err error
-
-	exp, err = otlpmetrichttp.New(
-		ctx,
-		otlpmetrichttp.WithTemporalitySelector(newRelicTemporalitySelector),
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	mp := sdkmetric.NewMeterProvider(
-		sdkmetric.WithReader(
-			sdkmetric.NewPeriodicReader(
-				exp,
-				sdkmetric.WithInterval(2*time.Second),
-			)))
-	otel.SetMeterProvider(mp)
-	return mp
-}
-
-func shutdownMetricProvider(
-	ctx context.Context,
-	mp *sdkmetric.MeterProvider,
-) {
-	// Do not make the application hang when it is shutdown.
-	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
-	defer cancel()
-	if err := mp.Shutdown(ctx); err != nil {
-		panic(err)
-	}
-}
-
-func newTraceProvider(
-	ctx context.Context,
-) *sdktrace.TracerProvider {
-
-	var exp sdktrace.SpanExporter
-	var err error
-
-	exp, err = otlptracehttp.New(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	// Instantiate a default resource with environment variables
-	r := resource.Default()
-
-	// Create trace provider
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithBatcher(exp),
-		sdktrace.WithResource(r),
-	)
-
-	// Set global trace provider
-	otel.SetTracerProvider(tp)
-
-	// Set trace propagator
-	otel.SetTextMapPropagator(
-		propagation.NewCompositeTextMapPropagator(
-			propagation.TraceContext{},
-			propagation.Baggage{},
-		))
-
-	return tp
-}
-
-func shutdownTraceProvider(
-	ctx context.Context,
-	tp *sdktrace.TracerProvider,
-) {
-	// Do not make the application hang when it is shutdown.
-	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
-	defer cancel()
-	if err := tp.Shutdown(ctx); err != nil {
-		panic(err)
-	}
-}
-
-type HttpWrapper struct {
-	operation            string
-	serverName           string
-	handler              http.Handler
-	httpServerDuration   metric.Float64Histogram
-	fibonacciInvocations metric.Int64Counter
-}
-
-func NewHttpWrapper(
-	handler http.Handler,
-	operation string,
-) http.Handler {
-
-	// Get service name from environment variables
-	serverName := os.Getenv("OTEL_SERVICE_NAME")
-
-	// Create HTTP server duration histogram
-	httpServerDuration, err := otel.GetMeterProvider().
-		Meter(serverName).
-		Float64Histogram("http.server.duration")
-	if err != nil {
-		log.Print(err.Error())
-		panic(err.Error())
-	}
-
-	// Create Fibonacci invocation counter
-	fibonacciInvocations, err := otel.GetMeterProvider().
-		Meter(serverName).
-		Int64Counter("fibonacci.invocations")
-	if err != nil {
-		log.Print(err.Error())
-		panic(err.Error())
-	}
-
-	// Initialize custom HTTP handler wrapper
-	w := HttpWrapper{
-		serverName:           serverName,
-		handler:              handler,
-		operation:            operation,
-		httpServerDuration:   httpServerDuration,
-		fibonacciInvocations: fibonacciInvocations,
-	}
-
-	return &w
-}
-
-// Implement custom ServeHTTP to wrap & track spans & metrics of the request
-func (h *HttpWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	requestStartTime := time.Now()
-
-	// Get context from request
-	ctx := r.Context()
-
-	// Set up trace attributes
-	startSpanAttributes := []trace.SpanStartOption{
-		trace.WithSpanKind(trace.SpanKindServer),
-		trace.WithAttributes(httpconv.ServerRequest(h.serverName, r)...),
-		trace.WithAttributes(semconv.NetHostName(h.serverName)),
-	}
-
-	// Start the server span
-	ctx, span := otel.GetTracerProvider().
-		Tracer("Fibonacci").
-		Start(ctx, r.Method+" /fibonacci", startSpanAttributes...)
-	defer span.End()
-
-	// Create response writer wrapper
-	rww := NewResponseWriterWrapper(w)
-	h.handler.ServeHTTP(rww, r.WithContext(ctx))
-
-	// Set up metric attributes
-	httpServerMetricAttributes := httpconv.ServerRequest(h.serverName, r)
-	fibonacciInvocationMetricAttributes := []attribute.KeyValue{}
-
-	if rww.statusCode > 0 {
-		// Add status code to metric attributes
-		httpServerMetricAttributes = append(
-			httpServerMetricAttributes,
-			semconv.HTTPStatusCode(rww.statusCode),
-		)
-		if rww.statusCode == 200 {
-			fibonacciInvocationMetricAttributes = append(
-				fibonacciInvocationMetricAttributes,
-				attribute.Bool("fibonacci.valid.n", true),
-			)
-		} else {
-			fibonacciInvocationMetricAttributes = append(
-				fibonacciInvocationMetricAttributes,
-				attribute.Bool("fibonacci.valid.n", false),
-			)
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
 		}
-
-		// Add status code to span attributes
-		endSpanAttributes := []attribute.KeyValue{semconv.HTTPStatusCode(rww.statusCode)}
-		span.SetAttributes(endSpanAttributes...)
+		shutdownFuncs = nil
+		return err
 	}
 
-	// Use floating point division here for higher precision (instead of Millisecond method).
-	elapsedTime := float64(time.Since(requestStartTime)) / float64(time.Millisecond)
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
 
-	h.fibonacciInvocations.Add(ctx, 1, metric.WithAttributes(fibonacciInvocationMetricAttributes...))
-	h.httpServerDuration.Record(ctx, elapsedTime, metric.WithAttributes(httpServerMetricAttributes...))
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up trace provider.
+	tracerProvider, err := newTraceProvider(ctx)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set up meter provider.
+	meterProvider, err := newMeterProvider(ctx)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	runtime.Start(runtime.WithMeterProvider(meterProvider))
+
+	// Set up logger provider.
+	loggerProvider, err := newLoggerProvider(ctx)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
+	global.SetLoggerProvider(loggerProvider)
+
+	return
 }
 
-// Wrapper for response writer in order to retrieve the status code of the HTTP call
-type responseWriterWrapper struct {
-	http.ResponseWriter
-	statusCode int
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
 }
 
-// Initialize HTTP response writer wrapper
-func NewResponseWriterWrapper(w http.ResponseWriter) *responseWriterWrapper {
-	return &responseWriterWrapper{w, http.StatusOK}
+func newTraceProvider(ctx context.Context) (*trace.TracerProvider, error) {
+	traceExporter, err := otlptracehttp.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(traceExporter),
+	)
+	return traceProvider, nil
 }
 
-// Wrapper method to intercept & store the HTTP status code
-func (rww *responseWriterWrapper) WriteHeader(code int) {
-	rww.statusCode = code
-	rww.ResponseWriter.WriteHeader(code)
+func newMeterProvider(ctx context.Context) (*metric.MeterProvider, error) {
+	metricExporter, err := otlpmetrichttp.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithReader(
+			metric.NewPeriodicReader(
+				metricExporter,
+				metric.WithInterval(3*time.Second),
+			),
+		),
+	)
+	return meterProvider, nil
+}
+
+func newLoggerProvider(ctx context.Context) (*log.LoggerProvider, error) {
+	logExporter, err := otlploghttp.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	loggerProvider := log.NewLoggerProvider(
+		log.WithProcessor(log.NewBatchProcessor(logExporter)),
+	)
+	return loggerProvider, nil
 }
